3984b4c51e77ff2a069d4063eb32c088141cf8a7
locpt-0253 <pham.tien.loc@sun-asterisk.com>
2022-11-25 20:35:22 +0700

    reorganize project

---
+++ AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,82 @@
+package AimsProject.src.hust.soict.dsai.aims.disc;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public DigitalVideoDisc(String title) {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public String toString() {
+        return this.id + ". DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + ": " + this.getCost() + "$";
+    }
+
+    public boolean isMatch(String title) {
+        if (this.title.equals(title)) return true;
+        return false;
+    }
+
+    public boolean isMatch(int id) {
+        if  (this.id == id) return true;
+        return false;
+    }
+}
